748795249b23a8c551b257694f37d04b1781f404
Guilherme S. B. <guilherme10205@gmail.com>
2025-03-06 15:49:11 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Nicolli, Eu te Amo. Se me amas também, sorria ;D</h1> 
+    return <h1>Nicolli, Eu te Amo. Se me amas também, sorria! ;D</h1> 
 }
 
 export default Home;
